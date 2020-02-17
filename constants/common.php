ee0d43c009791733d793bf43c91d89583d2efa9b
alexmasukov <alienspro2008@yandex.ru>
2020-02-18 00:28:17 +0900

    all meta and h1 remove from DB. All meta, h1, h2, h3, other titles, service name now get from constants 2

--- constants/common.php
+++ constants/common.php
@@ -1,9 +1,22 @@
 <?php
 
-$SERVICE_PAGE_TYPE = (object)array(
-    'CONNECTED' => 'CONNECTED',
-    'TOWN' => 'TOWN',
-    'OKRUG' => 'OKRUG',
-    'SERVICE' => 'SERVICE',
-    'FROM_MOSCOW_TO_B' => 'FROM_MOSCOW_TO_B'
-);
+class Constants {
+    const TITLE_PRICE_1 = '! Цены на грузоперевозки';
+    const TITLE_PRICE_2 = '! Цены на переезд';
+
+    const TITLE_SIMILAR_SERVICES_1 = '! Похожие услуги';
+
+    const TITLE_GALLERY_1 = '! Фото машин из нашего парка';
+
+    const GRUZOPEREVOZKI = '! Грузоперевозки';
+    const GRUZOVOE_TAKSI = '! Грузовое такси';
+    const VIVOZ_MEBELI = '! Вывоз мебели';
+    const KVARTIRNII_PEREEZD = '! Квартирный переезд';
+    const DACHNII_PEREEZD = '! Дачный переезд';
+    const OFISNII_PEREEZD = '! Офисный переезд';
+    const PEREVOZKA_MEBELI = '! Перевозка мебели';
+    const PEREVOZKA_PIANINO = '! Перевозка пианино';
+
+    const PEREEZDI = 'Переезды';
+    const TRANSPORTNIE_USLUGI = 'Транспортные услуги';
+}
